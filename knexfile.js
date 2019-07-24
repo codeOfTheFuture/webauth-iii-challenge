d59f90833cf86edaafbee2d7b09abefd9bf57d2d
Jeff Oliver <jeffoliver3384@gmail.com>
2019-07-24 16:40:29 -0400

    created db table for users in webauth-iii-challenge.db3

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './data/webauth-iii-db3',
+      filename: './data/webauth-iii-challenge.db3',
     },
     pool: {
       afterCreate: (conn, done) => {
